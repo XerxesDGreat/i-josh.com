b859160577aa7093b09aa856cc503c5227b52425
Josh Wickham <josh@i-josh.com>
2015-03-22 12:27:00 -0700

    fixing broken panorama page

--- themes/silk-pajamas/static/js/pano.js
+++ themes/silk-pajamas/static/js/pano.js
@@ -6,6 +6,22 @@ PanoJS.MSG_BEYOND_MAX_ZOOM = null;
 
 var panoViewer = null;
 
+var PanoConstants = {
+    _overlayName: 'panoOverlay',
+    _wrapperName: 'panoWrapper',
+    _containerName: 'panoContainer',
+    _headerName: 'panoHeader',
+    overlayName: function (justName) { return this.getName(this._overlayName, justName); },
+    wrapperName: function (justName) { return this.getName(this._wrapperName, justName); },
+    containerName: function (justName) { return this.getName(this._containerName, justName); },
+    headerName: function (justName) { return this.getName(this._headerName, justName); },
+    getName: function (baseName, justName) {
+        justName = typeof(justName) == 'undefined' ? false : justName;
+        var name = justName ? '' : '#';
+        return name + baseName;
+    }
+}
+
 function createViewer (panoInfo, baseUrl) {
     if (panoViewer) return;
   
@@ -13,7 +29,7 @@ function createViewer (panoInfo, baseUrl) {
     
     var myProvider = new PanoJS.TileUrlProvider('','','');
     myProvider.assembleUrl = function(xIndex, yIndex, zoom) {
-        return baseUrl + '/' + panoInfo.id + "/" + myPyramid.tile_filename( zoom, xIndex, yIndex );
+        return baseUrl + '/' + panoInfo.id + '/' + myPyramid.tile_filename( zoom, xIndex, yIndex );
     }
 
     var options = {
@@ -31,42 +47,42 @@ function createViewer (panoInfo, baseUrl) {
         options.initialZoom = panoInfo.initialZoom;
     }
     
-    panoViewer = new PanoJS("viewer", options);
+    panoViewer = new PanoJS('viewer', options);
 
     Ext.EventManager.addListener( window, 'resize', callback(panoViewer, panoViewer.resize) );
     panoViewer.init();
 };
 
 function initViewer(panoData) {
-	var panoContainer = $("#panoContainer");
-	var panoHeader = $("#panoHeader");
+	var panoContainer = $(PanoConstants.containerName());
+	var panoHeader = $(PanoConstants.headerName());
 
 	// clear out any existing pano
 	destroyViewer();
 	
-	panoContainer.height($('#overlay').dialog('option', 'height') - 70);
+	panoContainer.height($(PanoConstants.overlayName()).dialog('option', 'height') - 70);
 
 	// set the new one
 	if (!panoData) {
-		console.log("must pass pano information");
+		console.log('must pass pano information');
 		return
 	}
 	panoHeader.html(panoData.title);
 
-	var viewer = $("<div></div>");
+	var viewer = $('<div></div>');
 	viewer.attr({
-		id: "viewer",
-		style: "width: 100%; height: 100%",
-		class: "viewer"
+		id: 'viewer',
+		style: 'width: 100%; height: 100%',
+		class: 'viewer'
 	});
 	panoContainer.append(viewer);
 
-	createViewer(panoData, "/panorama/" + panoData.panoGroup);
+	createViewer(panoData, '/panorama/' + panoData.panoGroup);
 }
 
 function destroyViewer() {
-	$('#panoContainer').empty();
-	$('#panoHeader').empty();
+	$(PanoConstants.containerName()).empty();
+	$(PanoConstants.headerName()).empty();
 	panoViewer = null;
 }
 
@@ -74,8 +90,7 @@ function showOverlay() {
 	var panoData = $(this).data('panoInfo');
 	var dHeight = Math.ceil($(window).height() * .95);
 	var dWidth = Math.ceil($(window).width() * .95);
-	console.log('height: ' + dHeight + ', width: ' + dWidth);
-	$('#overlay').dialog({
+	$(PanoConstants.overlayName()).dialog({
 		modal: true,
 		draggable: false,
 		resizable: false,
@@ -88,7 +103,7 @@ function showOverlay() {
 
 function hideOverlay() {
 	destroyViewer();
-	$('#overlay')
+	$(PanoConstants.overlayName())
 		.height(null)
 		.hide();
 }
